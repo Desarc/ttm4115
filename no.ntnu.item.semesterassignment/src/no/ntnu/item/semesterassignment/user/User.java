84a45cc5640c128eb6390dbc705a985b3b39da58
Øyvin <desarc00@gmail.com>
2014-02-18 16:37:25 +0100

    some refactoring in progress

--- no.ntnu.item.semesterassignment/src/no/ntnu/item/semesterassignment/user/User.java
+++ no.ntnu.item.semesterassignment/src/no/ntnu/item/semesterassignment/user/User.java
@@ -6,22 +6,14 @@ import no.ntnu.item.arctis.runtime.Block;
 public class User extends Block {
 
 	public java.lang.String user_alias;
+	public container.TaxiMessage currentMessage;
 	
 	public static String getAlias(String alias) {
 		return alias;
 	}
-
-	public TaxiMessage generateRequest(String address) {
-		System.out.println("Generating new request for taxi to "+address);
-		return new TaxiMessage(TaxiMessage.DISPATCHER, this.user_alias, TaxiMessage.taxiRequest, address);
-	}
-
-	public String readMessage(TaxiMessage message) {
-		return ""+message.getType();
-	}
-
-	public String taxiRequested(String address) {
-		return "Taxi requested to "+address;
+	
+	public String getMessageType(TaxiMessage message) {
+		return message.getType();	
 	}
 	
 }
--- no.ntnu.item.semesterassignment/src/no/ntnu/item/semesterassignment/userclient/UserClient.java
+++ no.ntnu.item.semesterassignment/src/no/ntnu/item/semesterassignment/userclient/UserClient.java
@@ -1,7 +1,27 @@
 package no.ntnu.item.semesterassignment.userclient;
 
+import container.TaxiMessage;
 import no.ntnu.item.arctis.runtime.Block;
 
 public class UserClient extends Block {
+
+	public java.lang.String userId;
+
+	public TaxiMessage generateRequest(String address) {
+		System.out.println("Generating new request for taxi to "+address);
+		return new TaxiMessage(TaxiMessage.DISPATCHER, userId, TaxiMessage.taxiRequest, address);
+	}
+
+	public String handleQueueNo(TaxiMessage message) {
+		return "You are number "+message.getData1()+" in the queue.";
+	}
+	
+	public String handleConfirm(TaxiMessage message) {
+		return "Taxi reserved! "+message.getData1()+" will pick you up shortly.";
+	}
+
+	public String taxiRequested(String address) {
+		return "Taxi requested to "+address;
+	}
 
 }
