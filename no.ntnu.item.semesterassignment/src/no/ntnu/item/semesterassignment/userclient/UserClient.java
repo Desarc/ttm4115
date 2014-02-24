7e075fec0953a46fc395dcdc7d05cd5731fde9d5
Øyvin <desarc00@gmail.com>
2014-02-24 18:54:30 +0100

    only regular characters in addresses

--- no.ntnu.item.semesterassignment/src/no/ntnu/item/semesterassignment/userclient/UserClient.java
+++ no.ntnu.item.semesterassignment/src/no/ntnu/item/semesterassignment/userclient/UserClient.java
@@ -7,17 +7,17 @@ public class UserClient extends Block {
 
 	public java.lang.String userId;
 
-	public static final String NTNUGløs = "Høgskoleringen 1, 7034 Trondheim, Norge";
+	public static final String NTNUGløs = "Hogskoleringen 1, 7034 Trondheim, Norge";
 	public static final String NTNUDragvoll = "Loholt alle 85, 7049 Trondheim, Norge";
 	public static final String TrondheimSpektrum = "Klostergata 88, 7030 Trondheim, Norge";
 	public static final String MoholtStudBy = "Moholt Alle 3, 7050 Trondheim, Norge";
 	public static final String VollStudBy = "Vegamot 1, 7048 Trondheim, Norge";
 	public static final String Sirkus = "Thoning Owesens gate 31, 7044 Trondheim, Norge";
 	public static final String Granåsen = "Kongsvegen 204, 7026 Trondheim, Norge";
-	public static final String CitySyd = "Østre Rosten 28, 7075 Tiller, Norge";
+	public static final String CitySyd = "Ostre Rosten 28, 7075 Tiller, Norge";
 	public static final String IKEA = "Landbruksvegen 2, 7047 Trondheim, Norge";
 	public static final String TrondheimTorg = "Kongens gate 22B, 7011 Trondheim, Norge";
-	public static final String NidarosDomen = "Kongsgårdsgata 1, 7013 Trondheim, Norge";
+	public static final String NidarosDomen = "Kongsgardsgata 1, 7013 Trondheim, Norge";
 	
 	private String[] locations = {NTNUGløs, NTNUDragvoll, TrondheimSpektrum, MoholtStudBy, VollStudBy, Sirkus, Granåsen, CitySyd, IKEA, TrondheimTorg, NidarosDomen};
 
